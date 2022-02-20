170b87140733cbadf7e3228dca987707707aa13d
DevJo <sa02045@naver.com>
2022-02-20 23:58:48 +0900

    feat: add CatsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
-import { databaseProviders } from './database/database.provider';
-import { DatabaseModule } from './database/database.module';
+import { CatsModule } from './cats/cats.module';
+
 @Module({
-  imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: 'schema.gql',
-    }),
-    DatabaseModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService, ...databaseProviders],
+  imports: [CatsModule],
 })
 export class AppModule {}
--- src/database/database.provider.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import { MONGO_DB } from '../constants';
-
-export const databaseProviders = [
-  {
-    provide: 'DATABASE_CONNECTION',
-    useFactory: (): Promise<typeof mongoose> => mongoose.connect(MONGO_DB),
-  },
-];
